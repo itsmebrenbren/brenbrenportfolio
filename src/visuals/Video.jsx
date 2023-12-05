a98caa7418f42362c6e214b67951127c2d84828e
Brenna <brennalangille@Brennas-MacBook-Pro.local>
2023-12-05 13:50:59 -0500

    changed video card title

--- src/visuals/Video.jsx
+++ src/visuals/Video.jsx
@@ -8,7 +8,7 @@ import MyVideo from "./YouTube";
 export default function Video(){
     return(
         <Card className = "card my-2 border-black bg-romantic">
-            <CardHeader className = "h6 text-black text-center bg-brandy-rose">YouTube Video</CardHeader>
+            <CardHeader className = "h6 text-black text-center bg-brandy-rose">Portfolio Video</CardHeader>
             <CardBody className = "romantic d-flex justify-content-center align-items-center" style={{ minHeight: "300px" }}>
                 <MyVideo />
             </CardBody>
